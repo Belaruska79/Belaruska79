2f1d8af7b647b5d38e814b60df80787b6dd68005
Name <email@example.ru>
2020-12-20 11:40:54 +0100

    Initial commit

---
+++ Cat.py
@@ -0,0 +1,15 @@
+class Cat:
+    def__init__(self,name,sex,age):
+        self.name=name
+        self.sex=sex
+        self.age=age
+
+    def getName (self):
+        return self.name
+
+    def getSex (self):
+        return self.sex
+
+    def getAge(self):
+        return self.Age
+
